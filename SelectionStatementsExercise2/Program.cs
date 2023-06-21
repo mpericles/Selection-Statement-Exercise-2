6fff261e1e41d502191b8786720a3fd8f55e3908
mpericles <mpericles1@yahoo.com>
2023-06-21 16:57:45 -0400

    Selection-Statement-Exercise-2

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -1,10 +1,48 @@
-﻿namespace SelectionStatementsExercise2
+﻿using static System.Formats.Asn1.AsnWriter;
+using System.Linq.Expressions;
+using System.Reflection.Metadata;
+using System.Threading.Tasks;
+
+namespace SelectionStatementsExercise2
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Ask the user for their favorite school subject & //Store their answer in a variable
+            Console.WriteLine("Please input your favorite school subject");
+            Console.WriteLine("Please the full name of the subject as an example: Mathematics, English, Spanish, Science, physics, Chemistry...");
+            String userInput = Console.ReadLine();
+
+            //Pass the variable as the expression in a switch statement.
+            //Create 5 different cases for different subjects. Each case will print the subject chosen and a custom message of your choosing.
+
+            switch (userInput)
+            {
+                case "Mathematics":
+                    Console.WriteLine("You chose Mathematics. Your teacher is going to be Ms. Johnson");
+                    break;
+                case "English":
+                    Console.WriteLine("You chose English. Your teacher is going to be Ms. English");
+                    break;
+                case "Spanish":
+                    Console.WriteLine("You chose Spanish. Your teacher is going to be Ms. Spanish");
+                    break;
+                case "Science":
+                    Console.WriteLine("You chose Science. Your teacher is going to be Ms. Science");
+                    break;
+                case "Physics":
+                    Console.WriteLine("You chose hysics. Your teacher is going to be Ms. hysics");
+                    break;
+                default:
+                    // code block
+                    Console.WriteLine("Interesting pick!");
+                    Console.WriteLine("Call the Registrar to get your teacher name");
+                    break;
+            }
+
+            //Create a default case to handle any case not handled.
+
         }
     }
 }
